90c7fbb61651ff4e459b2d53952270bdbe3e3c45
clmarr <marr.54@buckeyemail.osu.edu>
2024-07-26 15:30:05 -0400

    made alphasorted version of SpanLLex

---
+++ LexTest.py
@@ -0,0 +1,24 @@
+#make alphabetized version of CatLLex and check for duplicates.
+
+import sys
+
+LEXLOC = "SpanLLex.txt" if len(sys.argv) <= 1 else sys.argv[1]
+
+o = open(LEXLOC)
+
+lines = [(ln+'$').split('$')[0].strip() for ln in o.readlines()]
+o.close()
+
+lines = [ln for ln in lines if ln != ''] 
+lines = sorted(lines)
+
+lexloc_prefix = LEXLOC[:LEXLOC.rindex(".")] if "." in LEXLOC else LEXLOC
+
+o = open(lexloc_prefix+"_alphasorted.txt",mode='w')
+
+for i in range(len(lines)-1):
+	o.write(lines[i] + "\n")
+	if lines[i] == lines[i+1]:
+		print("duplicate line at alphabetically sorted line number "+str(i)+": "+lines[i])
+o.write(lines[-1]+"\n")
+o.close()
