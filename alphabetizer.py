73419e398944f7d641d6cbd8c6acf99768406956
Clayton Marr <marr.54@buckeyemail.osu.edu>
2025-05-25 23:20:33 -0400

    various fixes including marking unmarked infinitive verbs in spanllex
    starting additions from REW

--- alphabetizer.py
+++ alphabetizer.py
@@ -1,21 +1,20 @@
-#make alphabetized version of CatLLex and check for duplicates.
+# make alphabetized version of file at LEX_LOC and check for duplicates.
 
-LEX_LOC = "SpanLLex_not_glitched.txt"
+LEX_LOC = "SpanLLex.txt"
 
-with open(LEX_LOC,mode='r',encoding='utf-8') as o:
-    #lines = [(ln+'$').split('$')[0].strip() for ln in o.readlines()]
+with open(LEX_LOC, mode='r', encoding='utf-8') as o:
+    # lines = [(ln+'$').split('$')[0].strip() for ln in o.readlines()]
     lines_with_comments = [ln.strip() for ln in o.readlines()]
     lines = [ln.split('$')[0].strip() for ln in lines_with_comments]
-
 
 lines = [ln for ln in lines if ln != '']
 lines = sorted(lines)
 
 lines_with_comments = sorted([ln for ln in lines_with_comments if ln != '' and ln[0] != '$'])
 
-with open("alphasorted.txt",mode='w',encoding='utf-8') as o:
-    for i in range(len(lines)-1):
+with open("alphasorted.txt", mode='w', encoding='utf-8') as o:
+    for i in range(len(lines) - 1):
         o.write(lines_with_comments[i] + "\n")
-        if lines[i] == lines[i+1]:
-            print("duplicate line at alphabetically sorted line number "+str(i)+": "+lines[i])
-    o.write(lines_with_comments[-1]+"\n")
+        if lines[i] == lines[i + 1]:
+            print("duplicate line at alphabetically sorted line number " + str(i) + ": " + lines[i])
+    o.write(lines_with_comments[-1] + "\n")
